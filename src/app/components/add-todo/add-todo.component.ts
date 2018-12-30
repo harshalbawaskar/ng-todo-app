278099410432d815815706d31ac02d464c797cc7
Harshal <bawaskarhp@gitlab.com>
2018-12-30 14:05:20 +0530

    Added form validation on add todo

--- src/app/components/add-todo/add-todo.component.ts
+++ src/app/components/add-todo/add-todo.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+
 import { Todo } from 'src/app/models/todo';
 import { TodosService } from 'src/app/services/todos.service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-todo',
@@ -9,21 +11,25 @@ import { Router } from '@angular/router';
   styleUrls: ['./add-todo.component.css']
 })
 export class AddTodoComponent implements OnInit {
-  public todo: Todo;
+  @ViewChild('todoForm') public todoForm: NgForm;
 
   constructor(private todosService: TodosService,
-    private router: Router) {
-    this.todo = new Todo();
-    this.todo.id = '12345' + (todosService.todos.length + 1);
-  }
+    private router: Router) { }
 
   ngOnInit() { }
 
   saveTodo() {
-    if (this.todo.title && this.todo.description) {
-      this.todosService.addTodo(this.todo);
-      this.router.navigate(['/todos']);
-    }
+    const todo = new Todo();
+    todo.id = '12345' + (this.todosService.todos.length + 1);
+    todo.title = this.todoForm.value.title;
+    todo.description = this.todoForm.value.description;
+
+    this.todosService.addTodo(todo);
+    this.router.navigate(['/todos']);
+  }
+
+  clear() {
+    this.todoForm.reset();
   }
 
   cancel() {
